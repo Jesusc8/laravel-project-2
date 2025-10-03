8bb53a676c5b0b5d3d60a47afb0194c7d05db8d5
Jesus Castro Java <jesusmcastror@gmail.com>
2025-10-03 11:58:04 +0200

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function blog()
+    {
+        // $posts = Post::get();
+
+        // $post=Post::first();
+
+        // $post=Post::find(25);
+
+        $posts=Post::latest()->paginate();
+
+
+
+        // dd($post);
+
+        return view('blog', ['posts' => $posts]);
+        
+    }
+
+    public function post(Post $post)
+    {
+
+        return view('post', ['post' => $post]); 
+        
+    }
+}
